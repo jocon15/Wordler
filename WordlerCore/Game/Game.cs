329a37acb2a19d77b7f60760ce4766f0e0674b8a
Jason O'Connell <jaywoc12@gmail.com>
2025-05-26 15:12:59 -0400

    added building board as you play

--- WordlerCore/Game/Game.cs
+++ WordlerCore/Game/Game.cs
@@ -2,13 +2,40 @@
 
 namespace WordlerCore.Game
 {
+
+	public class BoardTile
+	{
+		public readonly char Letter;
+
+		public readonly TileColor Color;
+
+		public BoardTile(char letter, TileColor color)
+		{
+			Letter = letter;
+			Color = color;
+		}
+	}
+
+	public class BoardRow
+	{
+		public readonly string Word;
+
+		public readonly List<BoardTile> Tiles;
+
+		public BoardRow(string word, List<BoardTile> tiles)
+		{
+			Word = word;
+			Tiles = tiles;
+		}
+	}
+
 	public class Game
 	{
 		public int Round { get; private set; } = 1;
 
-		private Filter.Filter _filter;
+		private List<BoardRow> _rows = new List<BoardRow>();
 
-		
+		private Filter.Filter _filter;
 		
 		public Game() 
 		{
@@ -19,8 +46,13 @@ namespace WordlerCore.Game
 		{
 			Round = 1;
 			_filter = new Filter.Filter(GameData.WORDS_JSON, GameData.WORDS);
+			ClearRows();
 		}
 
+		public List<BoardRow> GetRows()
+		{
+			return _rows;
+		}
 
 		public List<string> GetSuggestions()
 		{
@@ -38,12 +70,33 @@ namespace WordlerCore.Game
 		{
 			_filter.FilterRound(guessWord, guessWordColors);
 
+			AddRow(guessWord, guessWordColors);
+
 			Round++;
 		}
 
 		public int GetRemainingWords()
 		{
 			return _filter.GetRemainingWords();
+		}
+
+		private void AddRow(string word, List<TileColor> colors)
+		{
+			List<BoardTile> tiles = new List<BoardTile>();
+			BoardTile tile;
+			for (int i = 0; i < word.Length; i++)
+			{
+				tile = new BoardTile(word[i], colors[i]);
+				tiles.Add(tile);
+			}
+
+			BoardRow row = new BoardRow(word, tiles);
+			_rows.Add(row);
+		}
+
+		private void ClearRows()
+		{
+			_rows = new List<BoardRow>();
 		}
 	}
 }
