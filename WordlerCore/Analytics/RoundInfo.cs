a5dfa03ef5513071cdf91511751decd8cc872aa2
Jason O'Connell <jaywoc12@gmail.com>
2025-06-06 16:34:16 -0400

    added round analytics

---
+++ WordlerCore/Analytics/RoundInfo.cs
@@ -0,0 +1,24 @@
+﻿namespace WordlerCore.Analytics
+{
+	public class RoundInfo
+	{
+		public int RoundNumber { get; private set; }
+
+		public string GuessWord { get; private set; }
+
+		public int WordsEliminated { get; private set; }
+
+		public int WordsRemaining { get; private set; }
+
+		public long EliminationTimeMS { get; private set; }
+
+		public RoundInfo(int roundNumber, string guessWord, int wordsEliminated, int wordsRemaining, long eliminationTimeMS)
+		{
+			RoundNumber = roundNumber;
+			GuessWord = guessWord;
+			WordsEliminated = wordsEliminated;
+			WordsRemaining = wordsRemaining;
+			EliminationTimeMS = eliminationTimeMS;
+		}
+	}
+}
